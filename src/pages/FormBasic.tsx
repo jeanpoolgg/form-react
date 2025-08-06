aec1a7bd451a313de1faf71d2a7cd21998373b34
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-06 06:53:02 -0500

    feat(form-basic): create and configure FormBasic with react-hook-form

--- src/pages/FormBasic.tsx
+++ src/pages/FormBasic.tsx
@@ -9,12 +9,30 @@ import {
 	Text,
 	VStack,
 } from "@chakra-ui/react";
+import { Controller, type SubmitHandler, useForm } from "react-hook-form";
 import { useColorModeValue } from "@/components/ui/color-mode";
+
+type Inputs = {
+	name: string;
+	email: string;
+	password: string;
+	confirmPassword: string;
+	terms: boolean;
+};
 
 export function FormBasic() {
 	const cardBg = useColorModeValue("white", "gray.800");
 	const shadow = "0 1px 4px rgba(0,0,0,0.05)";
 	const shadowHover = "0 2px 8px rgba(0,0,0,0.08)";
+
+	const {
+		register,
+		control,
+		handleSubmit,
+		watch,
+		formState: { errors, isValid },
+	} = useForm<Inputs>();
+	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
 	return (
 		<VStack gap="6" textAlign="center">
@@ -46,65 +64,167 @@ export function FormBasic() {
 				w="full"
 				maxW="md"
 			>
-				<Fieldset.Root size="lg" as="form" onSubmit={(e) => e.preventDefault()}>
-					<Stack gap="4">
-						<Fieldset.Legend fontSize="xl" fontWeight="bold">
-							Registro
-						</Fieldset.Legend>
-						<Fieldset.HelperText>
-							Completa los campos para crear tu cuenta.
-						</Fieldset.HelperText>
-					</Stack>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Fieldset.Root size="lg">
+						<Stack gap="4">
+							<Fieldset.Legend fontSize="xl" fontWeight="bold">
+								Registro
+							</Fieldset.Legend>
+							<Fieldset.HelperText>
+								Completa los campos para crear tu cuenta.
+							</Fieldset.HelperText>
+						</Stack>
 
-					<Fieldset.Content mt="4">
-						{/* Nombre */}
-						<Field.Root>
-							<Field.Label>Nombre</Field.Label>
-							<Input name="name" placeholder="Tu nombre" />
-						</Field.Root>
+						<Fieldset.Content mt="4">
+							{/* Nombre */}
+							<Field.Root invalid={!!errors.name}>
+								<Field.Label>Nombre</Field.Label>
+								<Input
+									placeholder="Tu nombre"
+									{...register("name", { required: true, maxLength: 50 })}
+								/>
 
-						{/* Correo */}
-						<Field.Root>
-							<Field.Label>Correo electrónico</Field.Label>
-							<Input name="email" type="email" placeholder="tu@email.com" />
-						</Field.Root>
+								{errors.name?.type === "required" && (
+									<Field.ErrorText>Este campo es requerido</Field.ErrorText>
+								)}
+								{errors.name?.type === "maxLength" && (
+									<Field.ErrorText>
+										Este campo debe tener menos de 50 caracteres
+									</Field.ErrorText>
+								)}
+							</Field.Root>
 
-						{/* Contraseña */}
-						<Field.Root>
-							<Field.Label>Contraseña</Field.Label>
-							<Input name="password" type="password" placeholder="********" />
-						</Field.Root>
+							{/* Correo */}
+							<Field.Root invalid={!!errors.email}>
+								<Field.Label>Correo electrónico</Field.Label>
+								<Input
+									type="email"
+									placeholder="tu@email.com"
+									{...register("email", {
+										required: "Este campo es requerido",
+										maxLength: {
+											value: 50,
+											message: "Este campo debe tener menos de 50 caracteres",
+										},
+										pattern: {
+											value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+											message:
+												"Por favor, ingresa un correo electrónico válido",
+										},
+									})}
+								/>
 
-						{/* Confirmar Contraseña */}
-						<Field.Root>
-							<Field.Label>Confirmar Contraseña</Field.Label>
-							<Input
-								name="confirmPassword"
-								type="password"
-								placeholder="********"
-							/>
-						</Field.Root>
+								{errors.email?.type === "required" && (
+									<Field.ErrorText>{errors.email?.message}</Field.ErrorText>
+								)}
+								{errors.email?.type === "maxLength" && (
+									<Field.ErrorText>{errors.email?.message}</Field.ErrorText>
+								)}
+								{errors.email?.type === "pattern" && (
+									<Field.ErrorText>{errors.email?.message}</Field.ErrorText>
+								)}
+							</Field.Root>
 
-						<Checkbox.Root colorScheme="purple" mt="2">
-							<Checkbox.HiddenInput />
-							<Checkbox.Control />
-							<Checkbox.Label>Accept terms and conditions</Checkbox.Label>
-						</Checkbox.Root>
-					</Fieldset.Content>
+							{/* Contraseña */}
+							<Field.Root invalid={!!errors.password}>
+								<Field.Label>Contraseña</Field.Label>
+								<Input
+									type="password"
+									placeholder="********"
+									{...register("password", {
+										required: "Este campo es requerido",
+										minLength: {
+											value: 8,
+											message: "Este campo debe tener al menos 8 caracteres",
+										},
+									})}
+								/>
 
-					{/* Botón de registro */}
-					<Button
-						type="submit"
-						mt="8"
-						bg="#A83265"
-						color="white"
-						fontWeight="bold"
-						borderRadius="md"
-						_hover={{ bg: "#8C2855" }}
-					>
-						Registrarme
-					</Button>
-				</Fieldset.Root>
+								{errors.password?.type === "required" && (
+									<Field.ErrorText>{errors.password?.message}</Field.ErrorText>
+								)}
+								{errors.password?.type === "minLength" && (
+									<Field.ErrorText>{errors.password?.message}</Field.ErrorText>
+								)}
+							</Field.Root>
+
+							{/* Confirmar Contraseña */}
+							<Field.Root invalid={!!errors.confirmPassword}>
+								<Field.Label>Confirmar Contraseña</Field.Label>
+								<Input
+									type="password"
+									placeholder="********"
+									{...register("confirmPassword", {
+										required: "Este campo es requerido",
+										minLength: {
+											value: 8,
+											message: "Este campo debe tener al menos 8 caracteres",
+										},
+										validate: (value) =>
+											value === watch("password") ||
+											"Las contraseñas no coinciden",
+									})}
+								/>
+
+								{errors.confirmPassword?.type === "required" && (
+									<Field.ErrorText>
+										{errors.confirmPassword?.message}
+									</Field.ErrorText>
+								)}
+								{errors.confirmPassword?.type === "minLength" && (
+									<Field.ErrorText>
+										{errors.confirmPassword?.message}
+									</Field.ErrorText>
+								)}
+								{errors.confirmPassword?.type === "validate" && (
+									<Field.ErrorText>
+										{errors.confirmPassword?.message}
+									</Field.ErrorText>
+								)}
+							</Field.Root>
+							<Field.Root invalid={!!errors.terms}>
+								<Controller
+									name="terms"
+									control={control}
+									rules={{
+										required: "Debes aceptar los términos y condiciones",
+									}}
+									render={({ field }) => (
+										<Checkbox.Root
+											colorScheme="purple"
+											mt="2"
+											checked={!!field.value}
+											onCheckedChange={(e) => field.onChange(!!e.checked)}
+										>
+											<Checkbox.HiddenInput />
+											<Checkbox.Control />
+											<Checkbox.Label>
+												Acepto los términos y condiciones
+											</Checkbox.Label>
+										</Checkbox.Root>
+									)}
+								/>
+								{errors.terms && (
+									<Field.ErrorText>{errors.terms.message}</Field.ErrorText>
+								)}
+							</Field.Root>
+						</Fieldset.Content>
+
+						{/* Botón de registro */}
+						<Button
+							type="submit"
+							mt="8"
+							bg="#A83265"
+							color="white"
+							fontWeight="bold"
+							borderRadius="md"
+							_hover={{ bg: "#8C2855" }}
+							disabled={!isValid}
+						>
+							Registrarme
+						</Button>
+					</Fieldset.Root>
+				</form>
 			</Box>
 		</VStack>
 	);
