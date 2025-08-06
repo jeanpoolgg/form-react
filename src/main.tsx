12fd0716fbc95aab643b4e227e3a795141b9b66c
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-05 21:04:24 -0500

    feat(chakra): add and configure Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
+import { Provider } from "./components/ui/provider";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<Provider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
 	</StrictMode>,
 );
